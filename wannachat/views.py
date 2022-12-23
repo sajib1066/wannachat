ea8d90457c35d3a45ef8e7002e3421ff3c9324bc
Sajib Hossain <sajib1066@gmail.com>
2022-12-24 00:16:10 +0600

    setup home page

--- wannachat/views.py
+++ wannachat/views.py
@@ -1,10 +1,16 @@
 from django.views.generic import View
 from django.shortcuts import render
 
+from chatroom.models import Category
+
 
 class HomeView(View):
     """ Home view """
     template_name = 'home.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        category_list = Category.objects.all()
+        context = {
+            'category_list': category_list
+        }
+        return render(request, self.template_name, context)
