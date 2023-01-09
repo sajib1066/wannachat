5f825be0acb6e49ddb87a5aeff9096c427ee2c0a
Sajib Hossain <sajib1066@gmail.com>
2023-01-10 01:19:06 +0600

    update websocket disconnect method

--- chatroom/consumers.py
+++ chatroom/consumers.py
@@ -20,8 +20,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self):
         await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
+            self.room_group_name
         )
 
     # Receive message from WebSocket
