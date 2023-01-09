0f47fa4d338a98a5334f5f5230bbfa89f84042a6
Sajib Hossain <sajib1066@gmail.com>
2023-01-10 01:51:37 +0600

    update websocket configuration

--- wannachat/asgi.py
+++ wannachat/asgi.py
@@ -12,12 +12,12 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
+django.setup()
 from . import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wannachat.settings')
-django.setup()
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
