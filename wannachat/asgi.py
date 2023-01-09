3ebdd1751922c54ff80083d1eaf9156c5fe63097
Sajib Hossain <sajib1066@gmail.com>
2023-01-10 01:41:15 +0600

    update asgi settings

--- wannachat/asgi.py
+++ wannachat/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -17,6 +17,7 @@ from . import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wannachat.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
