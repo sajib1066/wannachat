a322266d00c2bf221a8b0877a02b2a5fd69051f8
Sajib Hossain <sajib1066@gmail.com>
2023-01-13 19:48:07 +0600

    update config

--- wannachat/settings.py
+++ wannachat/settings.py
@@ -101,11 +101,11 @@ ASGI_APPLICATION = 'wannachat.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        # 'BACKEND': 'channels.layers.InMemoryChannelLayer',
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('localhost', 6379)],
-        },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        # 'CONFIG': {
+        #     'hosts': [('localhost', 6379)],
+        # },
     }
 }
 
