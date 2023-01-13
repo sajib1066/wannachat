2765f5bcb9070451694ee0c70e6fc503502bfee0
Sajib Hossain <sajib1066@gmail.com>
2023-01-13 20:01:23 +0600

    update config

--- wannachat/asgi.py
+++ wannachat/asgi.py
@@ -12,8 +12,10 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 django.setup()
-from . import routing
+
+from wannachat import routing  # noqa
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wannachat.settings')
