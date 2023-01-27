348ef0cc120b875027b30661b312133d9fa6d21b
Sajib Hossain <sajib1066@gmail.com>
2023-01-27 18:07:57 +0600

    update sidebar

--- chatroom/views/chatroom.py
+++ chatroom/views/chatroom.py
@@ -30,6 +30,48 @@ class ChatRoomView(LoginRequiredMixin, View):
                 return redirect('home')
         chatrooms = ChatRoomUser.objects.filter(user=request.user)
         context = {
+            'user': request.user,
+            'obj': obj,
+            'chatrooms': chatrooms,
+            'messages': message_list,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        obj = get_object_or_404(self.model, pk=kwargs.get('pk'))
+        message_list = RoomMessage.objects.filter(room=obj)
+        photo = request.FILES.get('photo')
+        name = request.POST.get('name')
+        bio = request.POST.get('bio')
+        address = request.POST.get('address')
+        user = request.user
+        print(name, bio, address, user, photo)
+        if name:
+            user.profile.name = name
+        if bio:
+            user.profile.bio = bio
+        if address:
+            user.profile.address = address
+        if photo:
+            user.profile.avatar = photo
+        user.profile.save()
+        try:
+            ChatRoomUser.objects.get(
+                user=request.user,
+                room=obj
+            )
+        except ChatRoomUser.DoesNotExist:
+            if obj.current_users < obj.max_user:
+                ChatRoomUser.objects.create(
+                    user=request.user,
+                    room=obj
+                )
+            else:
+                messages.warning(request, "This room is full now.")
+                return redirect('home')
+        chatrooms = ChatRoomUser.objects.filter(user=request.user)
+        context = {
+            'user': request.user,
             'obj': obj,
             'chatrooms': chatrooms,
             'messages': message_list,
--- wannachat/urls.py
+++ wannachat/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from .views import HomeView
 
@@ -26,3 +28,10 @@ urlpatterns = [
     path('chatroom/', include('chatroom.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
+
+# serve media files in development environment --------------------------------
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
