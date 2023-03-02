1772533ff4de8d601a5c1ecf38903b748de88efb
Sajib Hossain <sajib1066@gmail.com>
2023-03-02 12:47:46 +0600

    update country model

--- chatroom/models/country.py
+++ chatroom/models/country.py
@@ -4,6 +4,7 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=220, unique=True)
     slug = models.SlugField(max_length=220, unique=True)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
