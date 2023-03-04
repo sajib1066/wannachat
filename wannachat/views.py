44be5862d76532e5b3752d9871ad712defef30ed
Sajib Hossain <sajib1066@gmail.com>
2023-03-04 17:37:38 +0600

    add dynamic country selection

--- wannachat/views.py
+++ wannachat/views.py
@@ -46,7 +46,34 @@ class ChangeCountryView(View):
     def get(self, request, *args, **kwargs):
         country = request.GET.get('country')
         request.session['country'] = country
-        return HttpResponse('Done')
+        print(country, '------------------------------------------')
+        try:
+            country_name = request.session['country']
+            print(country_name, '-------------------------------')
+            country = Country.objects.get(name=country_name)
+        except Exception as e:
+            print(e)
+            country = None
+        if country:
+            category_list = Category.objects.filter(
+                country=country, is_active=True
+            ).order_by('ordering')
+        else:
+            try:
+                country = Country.objects.get(pk=1)
+                category_list = Category.objects.filter(
+                    country=country, is_active=True
+                ).order_by('ordering')
+            except Country.DoesNotExist:
+                country = None
+                category_list = None
+        world_chat_list = Category.objects.filter(country__slug='world')
+        context = {
+            'world_chat_list': world_chat_list,
+            'category_list': category_list,
+            'selected_country': country,
+        }
+        return render(request, 'home/rooms.html', context)
 
 
 class FAQPageView(TemplateView):
