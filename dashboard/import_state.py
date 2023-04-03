c432325e28e3f7ff17e678f7ed4ac3035ea9fe15
Sajib Hossain <sajib1066@gmail.com>
2023-04-04 01:05:14 +0600

    update load state script

--- dashboard/import_state.py
+++ dashboard/import_state.py
@@ -10,9 +10,10 @@ def loadstate():
             counter += 1
             state_name = state['name']
             country_name = state['country_name']
-            # print(country_name, state_name)
             try:
                 country = Country.objects.get(name=country_name)
+                state = State.objects.get_or_create(country=country, name=state_name)
+                print(f"{counter} - {state}")
             except Country.DoesNotExist:
                 print(f"{country_name} is not found in country list")
         print(f"Total State: {counter}")
